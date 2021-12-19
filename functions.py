d1a7a129d037e34a5ff15f98da61d68b0ac757a7
IvanG <ionpopescu2106@gmail.com>
2021-12-19 02:35:14 +0100

    step 5

--- functions.py
+++ functions.py
@@ -24,3 +24,10 @@ def get_posts(data, tag):
         if f'#{tag}' in record['content']:
             results.append(record)
     return results
+
+
+def add_post(filename, post):
+    data = read_json(filename)
+    data.append(post)
+    with open('filename', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
